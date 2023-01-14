3e46b3fbb3c81fc3e5b934540960b59c64e310da
alpachm <alexpacheco407@gmail.com>
2023-01-14 16:18:39 -0500

    casi lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
-import { products } from "./data.js";
+let products = [
+  {
+    id: crypto.randomUUID(),
+    name: 'Hoodies',
+    price: '14.00',
+    stock: 10,
+    image: './assets/images/featured1.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Shirt',
+    price: '24.00',
+    stock: 15,
+    image: './assets/images/featured2.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Sweatshirts',
+    price: '24.00',
+    stock: 20,
+    image: './assets/images/featured3.png'
+  },
+
+]
 
 let iconMenu = document.querySelector('.bx-menu-alt-right');
 let menuMobile = document.querySelector('.menu__mobile');
@@ -10,119 +33,341 @@ let iconX = document.querySelector('.bx-x');
 let menuCart = document.querySelector('.menu__cart');
 let productosContainer = document.querySelector('.productos__container');
 let boxProducts = document.querySelector('.products');
-let emptyCart = document.querySelector('.empty__cart');
+let emptyCart = document.querySelector('.text__empty__cart');
+let productCartContainer = document.querySelector('.select_product_container');
+let totalCart = document.querySelector('.total__cart');
+let numCart = document.querySelector('.num__cart');
 
 let objCart = {};
 
 // PINTAR PRODUCTOS
 
 function printProducts() {
-    let html = '';
+  let html = '';
 
+  if (!products.length) {
+    productosContainer.innerHTML = `
+    <div class="empty__products">
+    <div class="img__empty__products">
+      <img src="./assets/images/empty-cart.png" alt="">
+    </div>
+
+    <div class="text__empty__products">
+      <h3>¡Sorry!, we don't have products in stock now</h3>
+    </div>
+  </div>
+    `
+  } else {
     products.forEach(({ id, name, price, stock, image }) => {
-        html += `
-        <div class="box__product">
-        <div class="img__product">
-          <img src="${image}" alt="">
-          <span id="${id}" class="btn_product"><i class='bx bx-plus btn__add'></i></span>
+      html += `
+          <div class="box__product">
+          <div class="img__product">
+            <img src="${image}" alt="">
+            <span id="${id}" class="btn_product"><i class='bx bx-plus btn__add'></i></span>
+          </div>
+          <div class="info__product">
+            <span>$${price} <p>| Stock: ${stock}</p></span>
+            <span>${name}</span>
+          </div>
         </div>
-        <div class="info__product">
-          <span>$${price} <p>| Stock: ${stock}</p></span>
-          <span>${name}</span>
-        </div>
-      </div>
-        `
+          `
 
-        productosContainer.innerHTML = html;
+      productosContainer.innerHTML = html;
     })
+  }
+
+
 }
 
 // PINTAR PRODUCTOS EN CARRITO
 
 function printSelectProductCart() {
-    let html = '';
+  let html = '';
 
-    let arrayCart = Object.values(objCart)
+  let arrayCart = Object.values(objCart)
+  let subTotal = 0;
+
+  if (!arrayCart.length) {
+    emptyCart.style.display = 'flex'
+    productCartContainer.style.display = 'none'
+
+
+  } else {
+    emptyCart.style.display = 'none'
+    productCartContainer.style.display = 'grid'
 
     arrayCart.forEach(({ amount, id, image, name, price, stock }) => {
-        html += `
-      <div id="${id}" class="select__product">
-      <div class="image__product">
-        <img src="${image}" alt="">
-      </div>
-      <div class="infoProduct">
-        <span>${name}</span>
-        <p>Stock: ${stock} | <span class="red">$${price}</span></p>
-        <p class="red">Subtotal: $24.00</p>
-        <div class="icons__selectProduct">
-          <i class='bx bx-minus'></i>
-          <p>${amount} units</p>
-          <i class='bx bx-plus'></i>
-          <i class='bx bx-trash-alt red'></i>
+
+      subTotal = price * amount
+
+      html += `
+        
+          <div class="select__product">
+          <div class="image__product">
+            <img src="${image}" alt="">
+          </div>
+          <div class="infoProduct">
+            <span>${name}</span>
+            <p>Stock: ${stock} | <span class="red">$${price}</span></p>
+            <p class="red">Subtotal: $${subTotal}.00</p>
+            <div id="${id}" class="icons__selectProduct">
+              <i class='bx bx-minus'></i>
+              <p>${amount} units</p>
+              <i class='bx bx-plus'></i>
+              <i class='bx bx-trash-alt red'></i>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-      `
+          `
     })
 
-    emptyCart.innerHTML = html;
+  }
+
+  productCartContainer.innerHTML = html;
+}
+
+function printTotalCart() {
+  let arrayCart = Object.values(objCart);
+
+  if (!arrayCart.length) {
+    totalCart.innerHTML = `
+  <div class="price__total">
+        <span>0 items</span>
+        <span>$0.00</span>
+      </div>
+      <button class="btn__of">Comprar</button>
+  `
+  } else {
+
+    let totalPrice = 0;
+    let totalAmount = 0;
+
+    arrayCart.forEach(({ amount, price }) => {
+      totalPrice += (price * amount)
+      totalAmount += amount
+    })
+
+    totalCart.innerHTML = `
+    <div class="price__total">
+          <span>${totalAmount} items</span>
+          <span>$${totalPrice}.00</span>
+        </div>
+        <button class="btn__of btn__buy">Comprar</button>
+    `
+  }
+}
+
+// NUMERO CARRITO 
+
+function printNumCart() {
+  let arrayCart = Object.values(objCart);
+  let totalAmount = 0;
+
+  arrayCart.forEach(({ amount }) => {
+    totalAmount += amount;
+  })
+
+  if (!arrayCart.length) {
+    numCart.style.display = 'none'
+  } else {
+    numCart.style.display = 'flex'
+    numCart.innerHTML = `${totalAmount}`
+  }
 }
 
 // MENU MOBILE
 
 iconMenu.addEventListener('click', function () {
-    menuMobile.classList.toggle('show__menu')
+  menuMobile.classList.toggle('show__menu')
 })
 
 // DARKMODE 
 
 moonIcon.addEventListener('click', function () {
-    body.className = 'dark__mode';
-    sunIcon.style.display = 'block';
-    moonIcon.style.display = 'none';
+  body.className = 'dark__mode';
+  sunIcon.style.display = 'block';
+  moonIcon.style.display = 'none';
 })
 
 sunIcon.addEventListener('click', function () {
-    body.className = 'body';
-    moonIcon.style.display = 'block';
-    sunIcon.style.display = 'none'
+  body.className = 'body';
+  moonIcon.style.display = 'block';
+  sunIcon.style.display = 'none'
 })
 
 // MENU CART
 
 iconCart.addEventListener('click', function () {
-    menuCart.classList.add('show__menu__cart');
+  menuCart.classList.add('show__menu__cart');
 })
 
 iconX.addEventListener('click', function () {
-    menuCart.classList.remove('show__menu__cart');
+  menuCart.classList.remove('show__menu__cart');
 })
 
 // LOGICA CARRITO 
 
 boxProducts.addEventListener('click', function (e) {
-    if (e.target.classList.contains('btn__add')) {
+  if (e.target.classList.contains('btn__add')) {
 
-        const id = e.target.parentElement.id;
+    const id = e.target.parentElement.id;
 
-        let selectProduct = products.find((item) => {
-            return item.id === id;
+    let selectProduct = products.find((item) => {
+      return item.id === id;
+    })
+
+    if (objCart[id]) {
+      if (selectProduct.stock <= objCart[id].amount) {
+        Swal.fire({
+          icon: 'error',
+          title: 'No hay más disponibles',
+          text: '¡Lo sentimos! No hay tenemos mas en stock',
         })
-
-        if (objCart[id]) {
-            objCart[id].amount++
-        } else {
-            objCart[id] = {
-                ...selectProduct,
-                amount: 1
-            }
-        }
+        return
+      } else {
+        objCart[id].amount++
+      }
+    } else {
+      objCart[id] = {
+        ...selectProduct,
+        amount: 1
+      }
     }
 
-    printSelectProductCart();
+  }
+
+  printSelectProductCart();
+  printTotalCart();
+  printNumCart();
 })
 
+menuCart.addEventListener('click', function (e) {
 
+  const id = e.target.parentElement.id
 
+  let selectProduct = products.find((item) => {
+    return item.id === id
+  })
+
+  if (e.target.classList.contains('bx-minus')) {
+    if (objCart[id].amount <= 1) {
+
+      Swal.fire({
+        title: '¿Esta seguro de eliminar este producto?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Eliminado',
+            'El producto ha sido eliminado',
+            'success',
+          ),
+            delete objCart[id]
+          printSelectProductCart();
+          printTotalCart();
+          printNumCart();
+        }
+      })
+
+    } else {
+      objCart[id].amount--
+    }
+
+  }
+
+  if (e.target.classList.contains('bx-plus')) {
+    if (objCart[id].amount >= selectProduct.stock) {
+      Swal.fire({
+        icon: 'error',
+        title: 'No hay más disponibles',
+        text: '¡Lo sentimos! No hay tenemos mas en stock',
+      })
+      return
+    } else {
+      objCart[id].amount++
+    }
+
+  }
+
+  if (e.target.classList.contains('bx-trash-alt')) {
+    Swal.fire({
+      title: '¿Esta seguro de eliminar este producto?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Eliminado',
+          'El producto ha sido eliminado',
+          'success',
+        ),
+          delete objCart[id]
+        printSelectProductCart();
+        printTotalCart();
+        printNumCart();
+      }
+    })
+  }
+
+  if (e.target.classList.contains('btn__buy')) {
+    let newArray = [];
+
+    Swal.fire({
+      title: '¿Estás seguro de realizar la compra?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          '¡Muchas gracias!',
+          'Su compra se ha realizado con exito',
+          'success'
+        ),
+          products.forEach((item) => {
+
+            if (item.id === objCart[item.id]?.id) {
+              newArray.push({
+                ...item,
+                stock: item.stock - objCart[item.id].amount
+              })
+            } else {
+              newArray.push(item)
+            }
+          })
+
+        products = newArray.filter((item) => {
+          if (!item.stock < 1) {
+            return item
+          }
+        })
+
+        objCart = {};
+
+        printProducts();
+        printSelectProductCart();
+        printTotalCart();
+        printNumCart();
+
+      }
+    })
+  }
+
+  printSelectProductCart();
+  printTotalCart();
+  printNumCart();
+})
 
 printProducts();
+printTotalCart();
+printNumCart();
+
